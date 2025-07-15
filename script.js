7d17bec9a317dc2031454d707e177a6b28db0fee
Prathik Mishra <126965610+Prathik-mishra@users.noreply.github.com>
2025-07-15 21:14:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 function firstWord(s) {
    s = s.trim();
-	
-	if ((s.startsWith('"') && s.endsWith('"')) ||
-      (s.startsWith("'") && s.endsWith("'"))) {
-    s = s.substring(1, s.length - 1);
-  }
+
+	while(s.startsWith('"') || s.endsWith('"')){
+        s = s.substring(1);
+     }
+      while (s.startsWith("'") || s.endsWith("'")) {
+     s = s.substring(1, s.length - 1);
+   }
 	s = s.trim();
 
 	let spaceIndex = s.indexOf(' ');
