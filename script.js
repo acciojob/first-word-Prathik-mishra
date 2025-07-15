4286ceb0cdeacee30ff269d8a8bad296c3daccfe
Prathik Mishra <126965610+Prathik-mishra@users.noreply.github.com>
2025-07-15 20:40:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 function firstWord(s) {
   // your code here
-	    if(s.includes(' ') ){
-            return s.split(" ")[0];
-        }
-        else{
-            return s;
-        }
+	     let trimmed = s.trimStart();
+	let spaceIndex = trimmed.indexOf(' ');
+ 
+  if (spaceIndex === -1) {
+    return trimmed;
+  }
+	
+  return trimmed.slice(0, spaceIndex);    
+  }
 }
 // Do not change the code below
 
