a4120fdfcf472dc3bd90c69cb5b42a211c8728d7
Prathik Mishra <126965610+Prathik-mishra@users.noreply.github.com>
2025-07-15 19:23:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 function firstWord(s) {
   // your code here
+	   s = s.split(" ")[0];
+        return s;
 }
-
 // Do not change the code below
 
 const s = prompt("Enter String:");
 alert(firstWord(s));
+
